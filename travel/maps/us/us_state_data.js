f9f1da74e0efe0c71b849eda3db1043269ef19af
thesourabh <sourabhshetty@gmail.com>
2018-07-15 01:06:12 -0400

    added data on hover, centered it a bit, and added a title

---
+++ travel/maps/us/us_state_data.js
@@ -0,0 +1,54 @@
+var states = {
+	AL: {
+		name: "Alabama",
+		dateVisited: "2018-03-05"
+	},
+	DC2: {
+		name: "Washington, District of Columbia",
+		dateVisited: "2017-09-28"
+	},
+	LA: {
+		name: "Louisiana",
+		dateVisited: "2018-03-06"
+	},
+	MD: {
+		name: "Maryland",
+		dateVisited: "2018-07-06"
+	},
+	MS: {
+		name: "Mississippi",
+		dateVisited: "2018-03-07"
+	},
+	NC: {
+		name: "North Carolina",
+		dateVisited: "2017-03-10"
+	},
+	NJ: {
+		name: "New Jersey",
+		dateVisited: "2017-12-27"
+	},
+	NY: {
+		name: "New York",
+		dateVisited: "2017-12-22"
+	},
+	PA: {
+		name: "Pennsylvania",
+		dateVisited: "2018-07-07"
+	},
+	SC: {
+		name: "South Carolina",
+		dateVisited: "2018-03-10"
+	},
+	TN: {
+		name: "Tennessee",
+		dateVisited: "2018-03-04"
+	},
+	VA: {
+		name: "Virginia",
+		dateVisited: "2017-08-17"
+	},
+	WV: {
+		name: "West Virginia",
+		dateVisited: "2017-12-27"
+	}
+}
