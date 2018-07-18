b64279e3a0286105216b58ee7fe3f7b2382ef8dc
thesourabh <sourabhshetty@gmail.com>
2018-07-18 02:44:45 -0400

    added india map

---
+++ travel/maps/india/state_data.js
@@ -0,0 +1,46 @@
+var states = {
+	AP: {
+		name: "Andhra Pradesh",
+		visited: " "
+	},
+	DL: {
+		name: "New Delhi",
+		passed: " "
+	},
+	GA: {
+		name: "Goa",
+		passed: " "
+	},
+	GJ: {
+		name: "Gujarat",
+		passed: " "
+	},
+	HP: {
+		name: "Gujarat",
+    visited: " "
+	},
+	HR: {
+		name: "Gujarat",
+		passed: " "
+	},
+	KA: {
+		name: "Karnataka",
+		visited: " "
+	},
+	MH: {
+		name: "Maharashtra",
+		visited: " "
+	},
+	MP: {
+		name: "Madhya Pradesh",
+		passed: " "
+	},
+	PB: {
+		name: "Madhya Pradesh",
+		passed: " "
+	},
+	UP: {
+		name: "Uttar Pradesh",
+		passed: " "
+	},
+}
