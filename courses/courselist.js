e08cc784efdfe3e11b73cb6a194be37407c15244
thesourabh <sourabhshetty@gmail.com>
2019-02-10 01:49:48 -0500

    spruced up courses page, updated colors and fonts in other mains

---
+++ courses/courselist.js
@@ -0,0 +1,117 @@
+const courseList = [
+  {
+    name: "Student Police Academy",
+    field: "Police",
+    number: "CINE 2064",
+    instructor: "Various",
+    school: "Virginia Tech Police Department"
+  },
+  {
+    name: "Introduction to Cinema Production",
+    field: "Cinema",
+    number: "CINE 2064",
+    instructor: "Charles Dye",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Introduction to Cinema",
+    field: "Cinema",
+    number: "CINE 2054",
+    instructor: "Stephen Prince",
+    school: "Virginia Tech",
+    enrollment: "Audit"
+  },
+  {
+    name: "Introductory Psychology",
+    field: "Psychology",
+    number: "PSYC 1004",
+    instructor: "Kurt Hoffman & E. Scott Geller",
+    school: "Virginia Tech",
+    enrollment: "Audit"
+  },
+  {
+    name: "Multimedia, Hypertext and Information Access",
+    field: "Computer Science",
+    number: "CS 4624",
+    instructor: "Edward Fox",
+    school: "Virginia Tech"
+  },
+  {
+    name: "System and Software Security",
+    field: "Computer Science",
+    number: "CS 5590",
+    instructor: "Matthew Hicks",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Independent Study",
+    field: "Computer Science",
+    number: "CS 5974",
+    instructor: "Eli Tilevich",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Advanced Topics in Software Engineering",
+    field: "Computer Science",
+    number: "CS 6704",
+    instructor: "Francisco Servant",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Special Study: Big Data Text Summarization",
+    field: "Computer Science",
+    number: "CS 5984",
+    instructor: "Edward Fox",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Network Security",
+    field: "Computer Science",
+    number: "CS 5584",
+    instructor: "J. Scot Ransbottom",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Virtual Environments",
+    field: "Computer Science",
+    number: "CS 5754",
+    instructor: "Doug Bowman",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Usability Engineering",
+    field: "Computer Science",
+    number: "CS 5714",
+    instructor: "Joe Gabbard",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Software Engineering",
+    field: "Computer Science",
+    number: "CS 5704",
+    instructor: "Na Meng",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Theory of Algorithms",
+    field: "Computer Science",
+    number: "CS 5114",
+    instructor: "Sharath Raghvendra",
+    school: "Virginia Tech"
+  },
+  {
+    name: "Information Visualization",
+    field: "Computer Science",
+    number: "CS 5764",
+    instructor: "Chris North",
+    school: "Virginia Tech"
+  }
+];
+
+
+const fieldColor = {
+  "Computer Science": "LimeGreen",
+  "Cinema": "OrangeRed",
+  "Police": "MediumBlue",
+  "Psychology": "HotPink",
+}
